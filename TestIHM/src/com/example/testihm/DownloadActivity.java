60079a0475ee412807ad7e38269334a6f2d19b6e
Hanane-AitLakbir <ait-lakbir.hanane@hotmail.fr>
2014-05-02 12:20:03 +0200

    Progress bar : download activity

--- TestIHM/src/com/example/testihm/DownloadActivity.java
+++ TestIHM/src/com/example/testihm/DownloadActivity.java
@@ -12,6 +12,7 @@ import utilities.Packet;
 import allocation.AllocationStrategy;
 import allocation.ChosenCloud;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -24,7 +25,6 @@ import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import coding.Coder;
 import coding.EmptyCoder;
 import connection.CloudNotAvailableException;
@@ -36,10 +36,10 @@ public class DownloadActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_download);
-		
+
 		Button testDownload = (Button) findViewById(R.id.testDownloadButton);
 		testDownload.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				ProviderCloud provider = new ProviderCloud("dropbox","dropbox");
@@ -55,11 +55,11 @@ public class DownloadActivity extends Activity {
 				}
 			}
 		});
-		
+
 		ListView listview = (ListView) findViewById(R.id.listDownload);
 		//populate the listvew with the name of files stored onto clouds
 		ArrayList<String> values = new ArrayList<String>();
-		
+
 		Metadata metadataListCloud = new JSonSerializer(Environment.getExternalStorageDirectory().getPath()+"/pip/metadata/file/files List.json").deserialize();
 		HashMap<String, String> map = metadataListCloud.getMap();
 		Iterator<String> iterator = map.keySet().iterator();
@@ -76,31 +76,42 @@ public class DownloadActivity extends Activity {
 		listview.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
+			public void onItemClick(final AdapterView<?> parent, View view, final int position,
 					long id) {
-				String fileName = parent.getItemAtPosition(position).toString();
-				AllocationStrategy strategy = new ChosenCloud();
-				String directory = Environment.getExternalStorageDirectory().getPath() + "/downloadPIP";
-				Coder coder = new EmptyCoder();
+
+				final ProgressDialog progress = ProgressDialog.show(DownloadActivity.this, getResources().getString(R.string.messageDownloading),"We (actually I am alone...) are downloading the file ...", true);
 				
-				try {
-					strategy.downLoad(fileName, directory, coder);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-				Toast.makeText(getApplicationContext(), "Download succesful", Toast.LENGTH_SHORT).show();
+				Thread t = new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						String fileName = parent.getItemAtPosition(position).toString();
+						AllocationStrategy strategy = new ChosenCloud();
+						String directory = Environment.getExternalStorageDirectory().getPath() + "/downloadPIP";
+						Coder coder = new EmptyCoder();
+
+						try {
+							strategy.downLoad(fileName, directory, coder);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						progress.dismiss();
+					}
+				});
+				t.start();
+			
+				//Toast.makeText(getApplicationContext(), "Downloading is successful", Toast.LENGTH_SHORT).show();
 			}
 		});
-		
+
 		//add button : return to previous activity
 		Button returnButton = (Button) findViewById(R.id.returnDownload);
 		returnButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				finish();
-				
+
 			}
 		});
 	}
