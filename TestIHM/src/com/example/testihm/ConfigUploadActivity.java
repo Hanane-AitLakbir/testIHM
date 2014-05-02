45733dce011149faf7e12513407806d70b9bd0d5
Hanane-AitLakbir <ait-lakbir.hanane@hotmail.fr>
2014-05-02 12:05:38 +0200

    Progress bar : upload activity

--- TestIHM/src/com/example/testihm/ConfigUploadActivity.java
+++ TestIHM/src/com/example/testihm/ConfigUploadActivity.java
@@ -11,6 +11,7 @@ import allocation.AllocationStrategy;
 import allocation.ChosenCloud;
 import allocation.InvalidParameterException;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -23,7 +24,6 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import coding.Coder;
 import coding.EmptyCoder;
 
@@ -33,13 +33,7 @@ public class ConfigUploadActivity extends Activity {
 	String fileToUpload;
 	int nbPackets=1; //default value 1 
 	ArrayList<String> chosenCloudsList = new ArrayList<String>();
-	//Context context;
-	//Activity me = this;
-	//attributes to manage the progress bar
-	//ProgressDialog progress;
-
-
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,7 +44,7 @@ public class ConfigUploadActivity extends Activity {
 		if(bundle!=null){
 			fileToUpload = bundle.getString("fileToUpload");
 		}
-
+		
 		TextView displayFileName = (TextView) findViewById(R.id.name_fileToUpload);
 		displayFileName.setText(fileToUpload);;
 
@@ -83,133 +77,53 @@ public class ConfigUploadActivity extends Activity {
 
 
 		//send button : creation an allocationStrategy object
-		Button send = (Button) findViewById(R.id.buttonSendFile);
+		final Button send = (Button) findViewById(R.id.buttonSendFile);
 		send.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				//Toast.makeText(getApplicationContext(), chosenCloudsList.toString(), Toast.LENGTH_SHORT).show();
-				//System.out.println(chosenCloudsList);
-				//System.out.println(fileToUpload);
-				//progress.show(context, "Please wait","We (actually I am alone...) are uploading the file ...", true);
-				//Toast.makeText(getApplicationContext(), "Please wait. \nWe (actually I am alone...) are uploading the file ...", Toast.LENGTH_LONG).show();
-				EditText inputNbPacket = (EditText) findViewById(R.id.inputChoiceNbPackets);
-				System.out.println("$"+inputNbPacket.getText().toString()+"$");
-				if(inputNbPacket.getText().toString().length()!=0){
-					nbPackets=Integer.parseInt(inputNbPacket.getText().toString());
-				}
-				System.out.println(nbPackets);
-				Coder coder = new EmptyCoder();
-				AllocationStrategy strategy = new ChosenCloud();
 
-				String[] clouds = new String[chosenCloudsList.size()];
-				int i=0;
-				for(String c : chosenCloudsList){
-					clouds[i]=c;
-					i++;
-				}
-				System.out.println(clouds.toString());
+				final ProgressDialog progress = ProgressDialog.show(ConfigUploadActivity.this, getResources().getString(R.string.messageSending),"We (actually I am alone...) are uploading the file ...", true);
+				
+				Thread t = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						EditText inputNbPacket = (EditText) findViewById(R.id.inputChoiceNbPackets);
+						System.out.println("$"+inputNbPacket.getText().toString()+"$");
+						if(inputNbPacket.getText().toString().length()!=0){
+							nbPackets=Integer.parseInt(inputNbPacket.getText().toString());
+						}
+						System.out.println(nbPackets);
+						Coder coder = new EmptyCoder();
+						AllocationStrategy strategy = new ChosenCloud();
 
-				try {
-					strategy.upLoad(fileToUpload, nbPackets,clouds, coder);
-					//System.out.println("boolean Config Upload :"+finished);
+						String[] clouds = new String[chosenCloudsList.size()];
+						int i=0;
+						for(String c : chosenCloudsList){
+							clouds[i]=c;
+							i++;
+						}
+						System.out.println(clouds.toString());
 
-					//sends message "Finished"
-					//					msg = mHandler.obtainMessage(MSG_UPLOAD_END);
-					//					mHandler.sendMessage(msg);
+						try {
+							strategy.upLoad(fileToUpload, nbPackets,clouds, coder);
 
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (InvalidParameterException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				//				Thread threadUpload = new Thread(new Runnable() {
-				//					Message msg = null;
-				//					public static final int MSG_UPLOAD = 0;
-				//					public static final int MSG_UPLOAD_END = 1;
-				//
-				//					final Handler mHandler = new Handler() {
-				//						public void handleMessage(Message msg) {
-				//							switch (msg.what) {
-				//							case MSG_UPLOAD:
-				//								progress = ProgressDialog.show(me, "Please wait","We (actually I am alone...) are uploading the file ...", true);
-				//								//				if (progress.isShowing()) {
-				//								//					progress.setMessage(((String) msg.obj));
-				//								//				}
-				//								//progress.show();
-				//								System.out.println("progress is showing ? ConfigUploadActivity :" + progress.isShowing());
-				//								break;
-				//							case MSG_UPLOAD_END:
-				//progress.dismiss();
-				//								break;
-				//							default:
-				//								break;
-				//							}
-				//						}
-				//					};
-				//
-				//					@Override
-				//					public void run() {
-				//
-				//						//sends message upload is launched
-				//						//Message msg = null;
-				//						msg = mHandler.obtainMessage(MSG_UPLOAD);
-				//						mHandler.sendMessage(msg);
-				//
-				//						EditText inputNbPacket = (EditText) findViewById(R.id.inputChoiceNbPackets);
-				//
-				//						nbPackets=Integer.parseInt(inputNbPacket.getText().toString());
-				//						System.out.println(nbPackets);
-				//						Coder coder = new EmptyCoder();
-				//						AllocationStrategy strategy = new ChosenCloud();
-				//
-				//						String[] clouds = new String[chosenCloudsList.size()];
-				//						int i=0;
-				//						for(String c : chosenCloudsList){
-				//							clouds[i]=c;
-				//							i++;
-				//						}
-				//						System.out.println(clouds.toString());
-				//
-				//						try {
-				//							boolean finished = strategy.upLoad(fileToUpload, nbPackets,clouds, coder);
-				//							System.out.println("boolean Config Upload :"+finished);
-				//
-				//							//sends message "Finished"
-				//							msg = mHandler.obtainMessage(MSG_UPLOAD_END);
-				//							mHandler.sendMessage(msg);
-				//
-				//						} catch (FileNotFoundException e) {
-				//							e.printStackTrace();
-				//						} catch (IOException e) {
-				//							e.printStackTrace();
-				//						} catch (InvalidParameterException e) {
-				//							// TODO Auto-generated catch block
-				//							e.printStackTrace();
-				//						}
-				//
-				//					}
-				//				});
-				//				threadUpload.start();
-				//				try {
-				//					threadUpload.join();
-				//				} catch (InterruptedException e) {
-				//					e.printStackTrace();
-				//				}
-
-				//progress.dismiss();
-				Toast.makeText(getApplicationContext(), "uploading is succesful", Toast.LENGTH_LONG).show();
-
-				finish();
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (InvalidParameterException e) {
+							e.printStackTrace();
+						}
+						progress.dismiss();
+						finish();
+					}
+				});
+				t.start();				
 			}
 		});
-
-
-
 	}
 
 	@Override
